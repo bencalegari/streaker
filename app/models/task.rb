daea66081999a879b53d270bf1d6911e0e12ce27
Ben Calegari <bencalegari@gmail.com>
2013-10-13 17:37:32 -0400

    Missed checkins processed correctly

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,32 +9,48 @@ class Task < ActiveRecord::Base
     @tasks = Task.where(user_id: user.id)
     @tasks.each do |task|
       task.delete_future_checkins
+      task.cleanup_checkins
       if task.last_checkin_creation < Time.now - 1.minute
         task.day_list.add(Time.now.strftime("%A")) # TESTING ONLY. THIS NEEDS TO BE FIXED.
-          task.day_list.each do |day|
-            binding.pry
-
-            CheckIn.create("task_id = task.id, start_time: #{time_creator(day)} ") #start_time: time_creator(day)
-          end
+        task.day_list.each do |day|
+          CheckIn.create(task_id: task.id, start_time: task.create_checkin_start_time(day), end_time: task.create_checkin_end_time(day))
+        end
+        task.last_checkin_creation = Time.now
       end
     end
   end
 
+  def cleanup_checkins
+    missed_check_ins = self.check_ins.where("end_time < ? AND state = ?", Time.now, "pending")
+    missed_check_ins.each do |check_in|
+      check_in.deny
+    end
+  end
+
   def delete_future_checkins
-    @check_ins = self.check_ins.where("start_time > ?", Time.now) 
-    @check_ins.each do |check_in|
+    future_check_ins = self.check_ins.where("start_time > ?", Time.now) 
+    future_check_ins.each do |check_in|
       check_in.delete    
     end
   end
 
-  def time_creator(day)
-    #Return datetime with:
-      #Year: Based on the day of the next time "day" happens
-      #Day: Based on the day next time day happens.. what the fuck.
-      #Hour: task.start_time.hour
-      #Minute: task.start_time.min
-  end
+  def create_checkin_start_time(day)
+    week = (Date.today .. Date.today + 6)
+      week.each do |day_of_week|
+        if day_of_week.strftime("%A") == day
+          return DateTime.new(day_of_week.year, day_of_week.month, day_of_week.day, self.start_time.hour, self.start_time.min )
+        end
+      end
+  end      
 
+  def create_checkin_end_time(day)
+    week = (Date.today .. Date.today + 6)
+      week.each do |day_of_week|
+        if day_of_week.strftime("%A") == day
+          return DateTime.new(day_of_week.year, day_of_week.month, day_of_week.day, self.end_time.hour, self.start_time.min )
+        end
+      end
+  end
 end
 
     
