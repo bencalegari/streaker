5fa0dbe2113f806c1edc29a66ef5692500bcb3f7
Ben Calegari <bencalegari@gmail.com>
2013-10-14 12:29:54 -0400

    Edited devise migration.

--- db/migrate/20130916231353_add_devise_to_users.rb
+++ db/migrate/20130916231353_add_devise_to_users.rb
@@ -1,5 +1,5 @@
 class AddDeviseToUsers < ActiveRecord::Migration
-  def self.change
+  def change
     create_table(:users) do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
