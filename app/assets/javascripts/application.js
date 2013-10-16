744c98bea59e18f5879b1662b3f7f929f1e5d56e
Ben Calegari <bencalegari@gmail.com>
2013-10-16 12:57:47 -0400

    Added raise 404 error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,9 +17,4 @@
 //= require chosen-jquery
 //= require_tree .
 
-$( document ).ready(function() {
-  $(function(){ $(document).foundation(); });
-
-  $(".notice").fadeOut(7000);
-  
-};
+$(function(){ $(document).foundation(); });
