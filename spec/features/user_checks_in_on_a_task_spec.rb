1a507f32123329d274b0cbd805cfcf8d447147b3
Ben Calegari <bencalegari@gmail.com>
2013-10-17 15:44:09 -0400

    Changed add_check_in test

--- spec/features/user_checks_in_on_a_task_spec.rb
+++ spec/features/user_checks_in_on_a_task_spec.rb
@@ -8,7 +8,7 @@ feature 'User checks in on a task', %Q{
   let(:user) { create(:user) }
   let!(:task) { create(:task, user: user) }
   let!(:check_in) { create(:check_in, task_id: task.id) }
-  let!(:bad_check_in) { create(:check_in, start_time: (Time.now - 2.hours), end_time:(Time.now - 1.hour) )}
+  let!(:bad_check_in) { create(:check_in, start_time: (Time.now + 1.hours), end_time:(Time.now + 2.hours) )}
 
   scenario 'user makes a check in on time' do
     sign_in_as(user)
@@ -20,12 +20,13 @@ feature 'User checks in on a task', %Q{
     expect(CheckIn.all.count).to eql(2)
   end
 
-  scenario 'user makes a checkin late or early' do
+  scenario 'user makes a checkin early' do
     sign_in_as(user)
     visit tasks_path  
     click_on "Check In"
 
-    expect(page).to have_content("You checked in!")
+    # expect(page).to have_content("You checked in!")
+    binding.pry
     expect(bad_check_in.state).to eql("pending")
     expect(CheckIn.all.count).to eql(2)
   end
